82f5d59cde121f3005f883efa05adda3232945a1
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2025-06-17 17:18:01 -0700

    remove header

--- api-reference/maestro/workflows/resumePausedWorkflow.sh
+++ api-reference/maestro/workflows/resumePausedWorkflow.sh
@@ -1,5 +1,4 @@
 #apx-snippet-start:resumePausedWorkflow
 curl --request POST "https://api-d.docusign.com/v1/accounts/${accountId}/workflows/${workflowId}/actions/resume" \
---header 'Content-Type: application/json' \
 --header "Authorization: Bearer ${accessToken}" \
 #apx-snippet-end:resumePausedWorkflow
